c7cf1dce60cb4e43b4a2ff80dad19c726fa037b9
mohammadali300696@gmail.com <mohammadali300696@gmail.com>
2024-06-04 02:28:20 +0600

    app state controlled

--- lib/core/cubits/app/app_cubit.dart
+++ lib/core/cubits/app/app_cubit.dart
@@ -1,16 +1,15 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+
 import 'package:product_show_case/core/model/user/user_data.dart';
-import 'package:product_show_case/core/services/navigation_service.dart';
-import 'package:product_show_case/core/services/service_locator.dart';
 import 'package:product_show_case/core/utils/db/shared_preference_helper.dart';
-import 'package:product_show_case/ui/router.dart';
 
 part 'app_state.dart';
 
 class AppCubit extends Cubit<AppState> {
-  final _navigationService = serviceLocator<NavigationService>();
-  AppCubit() : super(AppInitial());
+  AppCubit() : super(AppInitial()) {
+    loadApp();
+  }
 
   void loadApp() async {
     emit(AppInitial());
@@ -23,7 +22,6 @@ class AppCubit extends Cubit<AppState> {
       emit(LoadedAppState(
         isLoggedIn: isLoggedIn,
       ));
-      _navigationService.navigateToAndClearAll(RouteTo.loginPage);
       return;
     } else {
       emit(LoadedAppState(isLoggedIn: isLoggedIn));
